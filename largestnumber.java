bd5edbc10ff4dc4a5d21f111d7f799954377f80d
VISHALY216626 <svishaly123@gmail.com>
2017-06-07 11:19:08 +0530

    Create largestnumber.java

---
+++ largestnumber.java
@@ -0,0 +1,21 @@
+public class
+{
+Static void main(String[] args)
+{
+int x=23;
+int y=34;
+int z=90;
+if(x>y&&x>z)
+{
+System.out.println("The greatest number is: "+x);
+}
+elseif(y>x&&y>z)
+{
+System.out.println("The gratest number is: "+y);
+}
+else
+{
+System.out.println("The gratest number is: "+z);
+}
+}
+}
