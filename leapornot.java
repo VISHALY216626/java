303ca422fb2c5579711b0fe204af3bab4e7c190f
VISHALY216626 <svishaly123@gmail.com>
2017-06-07 11:30:38 +0530

    Create leapornot.java

---
+++ leapornot.java
@@ -0,0 +1,15 @@
+public class
+{
+Static void main(String[] args)
+{
+int n;
+if(n%4==0)
+{
+System.out.println("The year is leap");
+}
+else
+{
+System.out.println("the year is not leap");
+}
+}
+}
