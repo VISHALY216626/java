3cf0b6e54579fefccf8000754c38b4d932dea58b
VISHALY216626 <svishaly123@gmail.com>
2017-06-07 10:34:39 +0530

    Create vowelconsonant.java

---
+++ vowelconsonant.java
@@ -0,0 +1,16 @@
+public class
+{
+Static void main(String[] args)
+{
+char a;
+int b=(int)a;
+if(b==97||b==101||b==105||b==111||b==117)
+{
+System.out.println("The letter is vowel");
+}
+else
+{
+System.out.println("The letter is consonant");
+}
+}
+}
