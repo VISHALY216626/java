b54016541a7ee74d4f3e35aa7de26600ff7864b5
VISHALY216626 <svishaly123@gmail.com>
2017-06-07 11:07:42 +0530

    Create alphabetornot.java

---
+++ alphabetornot.java
@@ -0,0 +1,16 @@
+public class
+{
+Static void main(String[] args)
+{
+char a;
+int ascii=(int)a;
+if(ascii>=97&&ascii<=122||ascii>=65&&ascii<=90)
+{
+System.out.println("The letter is alphabet");
+}
+else
+{
+System.out.println("The letter is not alphabet");
+}
+}
+}
